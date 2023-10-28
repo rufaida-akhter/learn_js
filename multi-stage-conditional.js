e27b81611a1b97a33e8631745de1ffab02c901e4
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-29 03:20:07 +0600

    ok

---
+++ multi-stage-conditional.js
@@ -0,0 +1,15 @@
+var money = 100;
+var denishpeice = 45;
+var butterbread = 35;
+var tostPrice =20;
+if(denishpeice < money){
+    console.log("den mama ekta");
+}
+else if ( butterbread < money)
+{
+    console.log("ok den ");
+}
+else (tostPrice <money)
+{
+    console.log("ok ki r korar");
+}
---
+++ summary.js
@@ -0,0 +1,6 @@
+var destinations =["sajek","coxbazar","nepal","bali","sp"];
+console.log(destinations[2]);
+
+destinations.push("bd");
+console.log(destinations);
+console.log(destinations.length);
