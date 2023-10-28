f5959ec7efa0354711a04f1525b56c9a7e65bebc
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-29 02:30:53 +0600

    if else

---
+++ conditional.js
@@ -0,0 +1,14 @@
+var iphonePrice = 79000;
+var myBudget = 30;
+if (iphonePrice < myBudget){
+    console.log("ok buy it");
+}
+else{
+    console.log("o re");
+}
+
+var price = 6958;
+var mytk =9666;
+if(price<mytk || iphonePrice < myBudget){
+    console.log("ok do it");
+}
