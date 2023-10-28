38f786ba36fa05df43ce439cf5af61fda50efed4
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-28 16:34:15 +0600

    array

---
+++ array.js
@@ -0,0 +1,5 @@
+var frindsAge = [13, 12, 54, 78, 99, 78];
+console.log(frindsAge);
+var girlsName = ['akhi', 'pakhi' ,'rakhi ','sokhi ','laki'];
+console.log(girlsName);
+console.log(girlsName.length);
