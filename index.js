995049ae9fda8c7805486ec78cc46478fa751d07
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-28 16:53:46 +0600

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+var numbers = [45, 68, 78,56,89,];
+console.log(numbers[3]);
+var element = numbers[1];
+console.log(element);
+numbers[1]=50;
+console.log(numbers[1]);
+//1 find index of an element
+var position = numbers.indexOf(45);
+console.log(position);
