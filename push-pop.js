16fbcddff2625688a1c576e7549c7be1f5d41f9e
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-28 23:18:00 +0600

    push pop varname.push(value) varname.pop()

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+var num = [1,2,2,33,22,55,444,555];
+console.log(num);
+num.push (9965);
+console.log(num);
+num.pop();
+console.log(num);
+num.pop();
+console.log(num);
+num.pop();
+console.log(num);
+var element = num.pop();
+console.log(num);
+console.log(element);
